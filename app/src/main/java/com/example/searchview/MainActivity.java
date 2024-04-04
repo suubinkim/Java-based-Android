015cb583c992a8975f1f83c5b586e9324fcf279d
suubinkim <0vbbbv0@gmail.com>
2024-04-04 18:09:11 +0900

    viewBinding 미사용

--- app/src/main/java/com/example/searchview/MainActivity.java
+++ app/src/main/java/com/example/searchview/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -13,30 +14,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.searchview.databinding.ActivityMainBinding;
-
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding activityMainBinding;
+    //view의 주소 값 담을 변수
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //view의 주소값을 가져옴
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        ButtonClickListener buttonClickListener = new ButtonClickListener();
+        button.setOnClickListener(buttonClickListener);
+
         EdgeToEdge.enable(this);
-
-        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(activityMainBinding.getRoot());
-
-        ButtonClickListener1 buttonClickListener1 = new ButtonClickListener1();
-        activityMainBinding.button2.setOnClickListener(buttonClickListener1);
-
-        ButtonClickListener2 buttonClickListener2 = new ButtonClickListener2();
-        activityMainBinding.button3.setOnClickListener(buttonClickListener2);
-
-        CalendarListener1 calendarListener1 = new CalendarListener1();
-        activityMainBinding.calendarView.setOnDateChangeListener(calendarListener1);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -45,54 +43,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    class ButtonClickListener1 implements View.OnClickListener {
+    class ButtonClickListener implements View.OnClickListener {
 
-        // 날짜 설정
         @Override
         public void onClick(View v) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.YEAR, 2022);
-            calendar.set(Calendar.MONTH, 6);
-            calendar.set(Calendar.DAY_OF_MONTH, 20);
-
-            long date = calendar.getTimeInMillis();
-
-            activityMainBinding.calendarView.setDate(date);
-        }
-    }
-
-    class ButtonClickListener2 implements View.OnClickListener {
-
-        // 날짜 가져오기
-        @Override
-        public void onClick(View v) {
-            long date = activityMainBinding.calendarView.getDate();
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTimeInMillis(date);
-
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH) + 1;
-            int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-            activityMainBinding.textView4.setText(year + "/" + month + "/" + day);
-
-        }
-    }
-
-    // 캘린더 날짜 선택했을때
-    class CalendarListener1 implements CalendarView.OnDateChangeListener {
-
-        @Override
-        public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.YEAR, year);
-            calendar.set(Calendar.MONTH, month);
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
-            long date = calendar.getTimeInMillis();
-            activityMainBinding.calendarView.setDate(date);
-
-            activityMainBinding.textView5.setText(year + "/" + (month + 1) + "/" + dayOfMonth);
+            textView.setText("버튼을 눌렀습니다.");
         }
     }
 }
